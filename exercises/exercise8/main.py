da1bc826300905ea1eafe6bd1a6c9449eefdad8f
Sara Formoso <saraformoso@iMac-de-Sara.local>
2023-02-20 14:15:57 +0100

    ej 8

--- exercises/exercise8/main.py
+++ exercises/exercise8/main.py
@@ -3,3 +3,11 @@
 #   una con todas las letras minúsculas, otra con todas las letras mayúsculas y
 #   otra solo con la primera letra del nombre y de los apellidos en mayúscula.
 #   El usuario puede introducir su nombre combinando mayúsculas y minúsculas como quiera.
+
+print("nombre completo del usuario")
+
+nombre= input()
+print(nombre.upper())
+print( nombre.lower())
+print(nombre.title())
+
